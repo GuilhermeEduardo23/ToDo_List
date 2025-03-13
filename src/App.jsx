30fce5bc5bd29f27583b28c80949190b818f19ff
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2025-03-13 16:03:26 +0000

    feat: added filter component for task listings

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import Search from "./components/Search";
 import FormattedDate from "./components/Date";
+import Filter from "./components/Filter";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -29,9 +30,12 @@ function App() {
 
   const [search, setSearch] = useState("");
 
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
   const addTodo = (text, category) => {
     const newTodos = [
-      ...todos, 
+      ...todos,
       {
         id: Math.floor(Math.random() * 10000),
         text,
@@ -44,23 +48,25 @@ function App() {
   };
 
   const removeTodo = (id) => {
-  //Criando um novo array de todos, e preservando o original
-   const newTodos = [...todos];
+    //Criando um novo array de todos, e preservando o original
+    const newTodos = [...todos];
 
-   //Filtrando o array de todos, criando um novo array sem os todos removidos
-   const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+    //Filtrando o array de todos, criando um novo array sem os todos removidos
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
 
-   //Atualizando o estado de todos com o novo array
-   setTodos(filteredTodos);
-  }
+    //Atualizando o estado de todos com o novo array
+    setTodos(filteredTodos);
+  };
 
   const completeTodo = (id) => {
     const newTodos = [...todos];
-    newTodos.map(todo => (
-      todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
-    ));
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
     setTodos(newTodos);
-  }
+  };
 
   return (
     <div className="app">
@@ -68,13 +74,35 @@ function App() {
         <h1>Lista de Tarefas</h1>
         <FormattedDate />
       </div>
-      <Search search={search} setSearch={setSearch}/>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo_list">
-        {todos.filter(todo => todo.text.toLowerCase().includes(search.toLowerCase())).map(todo => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
-        ))}
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
-      <TodoForm addTodo={addTodo}/>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
