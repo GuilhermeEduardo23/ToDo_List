b1fa66e9e806cd7606c999ed6f10d3f9a484a894
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2025-03-16 18:23:33 +0000

    fix: added warning for when there is no task available.

--- src/App.jsx
+++ src/App.jsx
@@ -58,30 +58,34 @@ function App() {
       <Search search={search} setSearch={setSearch} />
       <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo_list">
-        {todos
-          .filter((todo) =>
-            filter === "All"
-              ? true
-              : filter === "Completed"
-              ? todo.isCompleted
-              : !todo.isCompleted
-          )
-          .filter((todo) =>
-            todo.text.toLowerCase().includes(search.toLowerCase())
-          )
-          .sort((a, b) =>
-            sort === "Asc"
-              ? a.text.localeCompare(b.text)
-              : b.text.localeCompare(a.text)
-          )
-          .map((todo) => (
-            <Todo
-              key={todo.id}
-              todo={todo}
-              removeTodo={removeTodo}
-              completeTodo={completeTodo}
-            />
-          ))}
+        {todos.length === 0 ? (
+          <h2 className="warning">Quais serão seus objetivos para hoje?</h2>
+        ) : (
+          todos
+            .filter((todo) =>
+              filter === "All"
+                ? true
+                : filter === "Completed"
+                ? todo.isCompleted
+                : !todo.isCompleted
+            )
+            .filter((todo) =>
+              todo.text.toLowerCase().includes(search.toLowerCase())
+            )
+            .sort((a, b) =>
+              sort === "Asc"
+                ? a.text.localeCompare(b.text)
+                : b.text.localeCompare(a.text)
+            )
+            .map((todo) => (
+              <Todo
+                key={todo.id}
+                todo={todo}
+                removeTodo={removeTodo}
+                completeTodo={completeTodo}
+              />
+            ))
+        )}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,22 @@
-const Todo = ({todo, removeTodo, completeTodo}) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div 
+    <div
       className="todo"
-      style={{textDecoration: todo.isCompleted ? 'line-through' : ""}}>
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
       <div className="content">
         <p>{todo.text}</p>
-        <p className="category"><span className="categoria_title">Categoria:</span> {todo.category}</p>
+        <p className="category">
+          <span className="categoria_title">Categoria:</span> {todo.category}
+        </p>
       </div>
       <div>
-        <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
-        <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}>
+          x
+        </button>
       </div>
     </div>
   );
